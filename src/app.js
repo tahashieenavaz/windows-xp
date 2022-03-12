dc19e5b2e91fb8a7f80f3d4ff68d1f273c1567e4
Taha Shieenavaz <tahashieenavaz@gmail.com>
2022-03-12 03:30:51 +0330

    Blue screen was completed. Windows XP default, waiting and hand cursor were added to the project.

--- src/app.js
+++ src/app.js
@@ -2,14 +2,30 @@
   setTimeout(() => {
     $("#black").style.display = "none";
     $("#blue").style.display = "flex";
-  }, 3000);
+    setTimeout(() => {
+      $("#blue").style.cursor = `url(assets/waiting.cur), auto`;
+    }, 1000);
+    setTimeout(() => {
+      $("#blue").style.cursor = `url(assets/default.cur), auto`;
+    }, 1000 + 500);
+    setTimeout(() => {
+      $("#blue").style.cursor = `url(assets/waiting.cur), auto`;
+    }, 1000 + 500 + 300);
+    setTimeout(() => {
+      $("#blue").style.cursor = `url(assets/default.cur), auto`;
+    }, 1000 + 500 + 300 + 200);
+    // TODO: We should add a start button for user to click
+    const loadingSound = new Audio("assets/loading.mp3");
+    $("#blue").addEventListener(
+      "mousemove",
+      (e) => {
+        loadingSound.play();
+      },
+      { once: true }
+    );
+  }, 1);
+
   function $(s) {
     return document.querySelector(s);
-  }
-
-  function animate(elements, endState, duration = 300) {
-    elements.forEach(($el) => {
-      $($el).animate([endState], { duration, fill: "forwards" });
-    });
   }
 })();
